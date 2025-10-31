f2502d1f779cde627bcb224cb96df2311223204c
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-31 20:16:17 +0600

    mongo connection with backend

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -19,10 +19,43 @@ async function run() {
     await client.connect();
     const userDB = client.db("userDataBase");
     const productsCollection = userDB.collection("products");
-    ////////why///////////////////////////$$$$$$$$$$$$$$
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.findOne(query)
+      res.send(result);
+    });
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
-      const result = await productsCollection.insetOne(newProduct);
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updateProduct.name,
+          price: updateProduct.price,
+        },
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
